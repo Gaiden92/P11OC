eb69ae37b479f87de542ce931986771eb8722106
Gaiden92 <samy_fouchal@hotmail.fr>
2024-01-19 19:14:19 +0100

    Correction bug reservation plus de 12 places par compétitions.

--- server.py
+++ server.py
@@ -46,6 +46,10 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    if placesRequired > 12:
+        flash("You cannot book more than 12 for one competition, please try again.")
+        return render_template('booking.html', club=club, competition= competition)
+    
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
