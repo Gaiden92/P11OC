0f7f3b68326090cb63240067a21ca5fe29886dde
Gaiden92 <samy_fouchal@hotmail.fr>
2024-01-27 12:36:06 +0100

    Implémentation du dashboard page d'accueil

--- server.py
+++ server.py
@@ -18,11 +18,26 @@ def loadBooking():
         listBookings = json.load(books)
         return listBookings
 
+def update_booking(places_required:int, club:dict, competition:dict) -> bool:
+        int_club_points = int(bookings["clubs"][club['name']][competition["name"]])
+        int_new_club_points = int_club_points + places_required
+        if club_book_more_than_twelve_places(int_new_club_points):
+            return False
+        else:
+            bookings["clubs"][club['name']][competition["name"]] = str(int_new_club_points)
+            with open('bookings.json', "w") as file:
+                json.dump(bookings, file, indent=4)
+                return True
+def club_book_more_than_twelve_places(nb_places):
+    return nb_places > 12
+
+
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+bookings = loadBooking()
 
 @app.route('/')
 def index():
@@ -51,12 +66,13 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+
+    # Mise à jour des places des clubs par compétitions 
+    if update_booking(placesRequired, club, competition):
+        flash('Great-booking complete!')
+    else:
+        flash("You cannot book more than 12 places in the same competition.")
     return render_template('welcome.html', club=club, competitions=competitions)
-
-
-# TODO: Add route for points display
 
 
 @app.route('/logout')
