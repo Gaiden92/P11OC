afa4f776f710ee005d49ccc104bae45c7cfe281b
Gaiden92 <samy_fouchal@hotmail.fr>
2024-02-04 16:10:02 +0100

    Implémentation test test_isCompetitionClose

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,10 @@
-import pytest
-
+import pytest, json
 from server import create_app, isCompetitionClose
 
 @pytest.fixture
 def app():
     app_instance = create_app({"TESTING": True})
     with app_instance.app_context():
-        isCompetitionClose
         yield app_instance
 
 @pytest.fixture
@@ -14,3 +12,57 @@ def client(app):
     with app.test_client() as client:
         with app.app_context():
             yield client
+
+@pytest.fixture
+def clubs_data():
+    with open("clubs.json") as file:
+        clubs = json.load(file)
+        return clubs
+
+@pytest.fixture
+def competitions_data():
+    with open("competitions.json") as file:
+        competitions = json.load(file)
+        return competitions
+
+@pytest.fixture
+def bookings_data():
+    return {
+        "clubs": {
+            "She Lifts": {
+                "Spring Festival": "12",
+                "Fall Classic": "2"
+            },
+            "Iron Temple": {
+                "Spring Festival": "10",
+                "Fall Classic": "5"
+            },
+            "Simply Lift": {
+                "Spring Festival": "1",
+                "Fall Classic": "9"
+            }
+        }
+}
+
+
+@pytest.fixture
+def load_clubs_and_competitions_and_bookings(app, clubs_data, competitions_data, bookings_data):
+    with app.app_context():
+        app.clubs = clubs_data['clubs']
+        app.competitions = competitions_data['competitions']
+        app.bookings = bookings_data['clubs']
+        yield
+
+@pytest.fixture
+def club_competition_test_open_or_close(app, load_clubs_and_competitions_and_bookings):
+    competition_open = [competition for competition in app.competitions
+                   if competition['name'] == "Spring Festival"][0]
+    competition_close = [competition for competition in app.competitions
+                   if competition['name'] == "Fall Classic"][0]
+
+    club = [club for club in app.clubs
+                   if club['name'] == "Simply Lift"][0]
+    app.club = club
+    app.competition_open = competition_open
+    app.competition_close = competition_close
+    yield
--- tests/units/test_isCompetitionClose.py
+++ tests/units/test_isCompetitionClose.py
@@ -1,10 +1,29 @@
 from tests.conftest import isCompetitionClose
 
 
-def test_saveCompetitions(app):
-    competition_date = "2024-03-27 10:00:00"
-    close = isCompetitionClose(competition_date)
-    expected = False
+def test_book_competition_open(client, app, club_competition_test_open_or_close):
+    club = app.club
+    competition = app.competition_open
+    response = client.get(f'/book/{competition["name"]}/{club["name"]}')
+    competition_date = competition["date"]
+    is_close = isCompetitionClose(competition_date)
 
+    # Test du status code
+    assert response.status_code == 200
     # Vérifiez si la compétition est clôturée
-    assert close == expected
+    assert is_close == False
+
+def test_book_competition_close(client, app, club_competition_test_open_or_close):
+    club = app.club
+    competition = app.competition_close
+    response = client.get(f'/book/{competition["name"]}/{club["name"]}')
+    competition_date = competition["date"]
+    is_close = isCompetitionClose(competition_date)
+
+    # Test du status code
+    assert response.status_code == 200
+    # Vérifiez si la compétition est clôturée
+    assert is_close == True
+
+    print(response.data.decode())
+
